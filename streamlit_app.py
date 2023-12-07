2a52980607f762b15aebd140b1a1dbfee1c5a149
Fabrizio Sgro <117785876+Faabrisgro@users.noreply.github.com>
2023-12-07 16:55:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 from PIL import Image 
-from urllib.request import urlopen streamlit.image( Image.open(urlopen(df2[0][0])), width = 400, caption=product_caption )
+from urllib.request import urlopen 
+
+streamlit.image( Image.open(urlopen(df2[0][0])), width = 400, caption=product_caption )
 
 import streamlit 
 
